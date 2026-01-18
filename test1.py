2651bf34c49341fd65478700f1236cb8f8b3ba54
MohammedHassan11 <mh9786636@gmail.com>
2026-01-18 20:01:15 +0200

    Remove large output video and test file

--- test1.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-import cv2
-
-model = YOLO("yolov8n.pt")
-img = cv2.imread(r"C:\TERM 7\computer vision\final project\data\CarDD_YOLO\images\train\000009.jpg")
-
-results = model(img, conf=0.25)
-results[0].show()
--- test2.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-import cv2
-
-damage_model = YOLO(r"C:\TERM 7\computer vision\final project\data\CarDD_YOLO\runs\detect\train2\weights\best.pt")
-img = cv2.imread(r"C:\Users\mh978\Videos\Captures\Media Player 12_17_2025 11_44_01 AM.png")
-
-results = damage_model(img, conf=0.1)
-results[0].show()
